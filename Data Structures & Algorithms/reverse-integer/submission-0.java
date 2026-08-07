ffcb8c6aac6efcc84de6df0415e57a53d7994673
Ranjeet <ranjitkumar123bind@gmail.com>
2026-08-07 20:41:05 +0530

    Add: reverse-integer - submission-0

---
+++ Data Structures & Algorithms/reverse-integer/submission-0.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int reverse(int x) {
+       
+        long reversed=0;
+        while(x != 0){
+            int remainder=x%10;
+            reversed=(reversed *10)+ remainder;
+            x=x/10;
+        }
+        if(reversed > Integer.MAX_VALUE || reversed < Integer.MIN_VALUE){
+            return 0;
+        }
+        return (int)reversed;
+    }
+}
